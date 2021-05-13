679af31c8e460177cfb929f29ddf944e1f34ff11
TEMFACK DERICK <dericktemfack@gmail.com>
2021-05-13 03:52:55 +0100

    10.13.2020

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ColorComponent } from './color/color.component';
+import { HeaderComponent } from './header/header.component';
+import { RightSidebarComponent } from './right-sidebar/right-sidebar.component';
+import { LeftSidebarComponent } from './left-sidebar/left-sidebar.component';
+import { HeaderLeftComponent } from './header/header-left/header-left.component';
+import { HeaderRightComponent } from './header/header-right/header-right.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ColorComponent
+    ColorComponent,
+    HeaderComponent,
+    RightSidebarComponent,
+    LeftSidebarComponent,
+    HeaderLeftComponent,
+    HeaderRightComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
